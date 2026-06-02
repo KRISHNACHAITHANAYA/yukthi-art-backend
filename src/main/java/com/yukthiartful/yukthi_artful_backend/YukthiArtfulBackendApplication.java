a2c3e405263cfaa04380d4ae1e608813b667d33f
Krishna Chaithanya <your-email@example.com>
2026-06-02 21:43:55 +0530

    Innitial commit

---
+++ src/main/java/com/yukthiartful/yukthi_artful_backend/YukthiArtfulBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yukthiartful.yukthi_artful_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YukthiArtfulBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YukthiArtfulBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yukthiartful/yukthi_artful_backend/YukthiArtfulBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukthiartful.yukthi_artful_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YukthiArtfulBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
